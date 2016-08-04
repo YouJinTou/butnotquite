f73ad81b2f25b41539cb9edd69b0ab41338c5977
YouJinTou <dandreevd@gmail.com>
2016-08-04 20:34:23 +0300

    Added a simplistic graphic user interface

---
+++ Client/Game.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace Client
+{
+    using butnotquite.Models;
+    using butnotquite.Defaults;
+    using ViewModel;
+
+    using System.Collections.ObjectModel;
+    using System.Windows;
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.InitializeGame();        
+        }
+
+        private void InitializeGame()
+        {
+            Chessboard initialChessboard = new Chessboard(false);
+            ObservableCollection<PieceViewModel> pieces = new ObservableCollection<PieceViewModel>();
+
+            for (int square = 0; square < initialChessboard.Board.Length; square++)
+            {
+                int x = square % 8;
+                int y = square / 8;
+                Point position = new Point(x, y);
+                PieceViewModel pieceModel = new PieceViewModel()
+                {
+                    PieceType = initialChessboard.Board[square].OccupiedBy.Type,
+                    Color = initialChessboard.Board[square].OccupiedBy.Color,
+                    Position = position
+                };
+
+                if (pieceModel.PieceType == PieceType.None)
+                {
+                    continue;
+                }
+
+                pieces.Add(pieceModel);
+            }
+
+            this.ChessboardUI.ItemsSource = pieces;
+        }
+    }
+}
